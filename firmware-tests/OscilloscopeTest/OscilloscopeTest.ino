9dfd4ba3bc9eebce904ce587a4707388ba0e0998
RobotFreak <p.recktenwald@gmail.com>
2020-08-24 22:40:38 +0200

    OscilloscopeTest example

---
+++ firmware-tests/OscilloscopeTest/OscilloscopeTest.ino
@@ -0,0 +1,71 @@
+#include <ILI9341_t3.h>
+#include <font_Arial.h> // from ILI9341_t3
+#include <SPI.h>
+#include "oscilloscope.h"
+
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+AudioPlaySdWav           playSdWav1;
+Oscilloscope             scope;     
+AudioOutputI2S           i2s1;
+AudioConnection          patchCord1(playSdWav1, 0, i2s1, 0);
+AudioConnection          patchCord2(playSdWav1, 1, i2s1, 1);
+AudioConnection          patchCord3(playSdWav1, 0, scope, 0);
+AudioControlSGTL5000     sgtl5000_1;
+
+// Use these with the Teensy Audio Shield
+#define SDCARD_CS_PIN    10
+#define SDCARD_MOSI_PIN  7
+#define SDCARD_SCK_PIN   14
+
+// For optimized ILI9341_t3 library
+#define TFT_DC      5
+#define TFT_CS      4
+#define TFT_RST     255  // 255 = unused, connect to 3.3V
+
+//ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC, TFT_RST, TFT_MOSI, TFT_SCLK, TFT_MISO);
+ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC);
+
+void enableScope(boolean enable) {
+  enable ? scope.ScreenSetup(&tft) : scope.ScreenSetup(NULL);
+}
+
+void setup() {
+  Serial.begin(115200);
+  tft.begin();
+  tft.setRotation(3);
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setFont(Arial_24);
+  //tft.setTextSize(3);
+  //tft.setCursor(40, 8);
+  //tft.println("Oscillosope");
+
+  AudioMemory(8);
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(0.5);
+  enableScope(true);
+
+  //SPI.setMOSI(SDCARD_MOSI_PIN);
+  //SPI.setSCK(SDCARD_SCK_PIN);
+  if (!(SD.begin(SDCARD_CS_PIN))) {
+    while (1) {
+      Serial.println("Unable to access the SD card");
+      delay(500);
+    }
+  }
+  delay(1000);
+
+}
+
+void loop() {
+  if (playSdWav1.isPlaying() == false) {
+    Serial.println("Start playing");
+    playSdWav1.play("SDTEST2.WAV");
+    delay(10); // wait for library to parse WAV info
+  }
+  scope.update();
+}
